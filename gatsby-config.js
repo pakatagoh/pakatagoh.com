75185be1487eb73f2e9e9dfba6471a23eaf3e61d
Pakata Goh <pakatagohlh@gmail.com>
2019-07-22 22:19:32 +0800

    update gatsby config sitemeta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Pakata Goh`,
+    description: `My personal site where I drop blog posts, projects... that kinda stuff`,
+    author: `Pakata Goh`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Pakata Goh`,
+        short_name: `Pakata`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
