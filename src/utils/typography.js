7ee66fa214b3b03a532478340e96a89a1716d6f2
Pakata Goh <pakatagohlh@gmail.com>
2019-08-05 20:30:44 +0800

    update gitignore to ignore font files

---
+++ src/utils/typography.js
@@ -0,0 +1,8 @@
+import Typography from 'typography';
+const typography = new Typography({
+  baseFontSize: '18px',
+  baseLineHeight: 1.666,
+  headerFontFamily: ['Bebas Neue', 'Segoe UI', 'Arial', 'sans-serif'],
+  bodyFontFamily: ['Roboto', 'Georgia', 'serif'],
+});
+export default typography;
