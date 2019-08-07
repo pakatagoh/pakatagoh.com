7b4c0dc9c49f4d96413f5d87907fbe9ca320fd57
Pakata Goh <pakatagohlh@gmail.com>
2019-08-08 00:00:47 +0800

    add fonts file and update font imports in GlobalStyles

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,47 +1,42 @@
 import { createGlobalStyle } from 'styled-components';
-import BebasNeueBold from '../assets/fonts/BebasNeue-Bold.ttf';
-import RobotoBold from '../assets/fonts/Roboto-Bold.ttf';
-import RobotoMedium from '../assets/fonts/Roboto-Medium.ttf';
-import Roboto from '../assets/fonts/Roboto-Regular.ttf';
-import RobotoCondensedRegular from '../assets/fonts/RobotoCondensed-Regular.ttf';
-import RobotoCondensedRegularItalic from '../assets/fonts/RobotoCondensed-RegularItalic.ttf';
+import * as fontFiles from './fonts';
 
 const GlobalStyle = createGlobalStyle`
-    @font-face {
+  @font-face {
     font-family: 'Bebas Neue';
     font-weight: 'bold';
-    src: url(${BebasNeueBold})  format('truetype'),
+    src: url(${fontFiles.BebasNeueBold}) format('truetype');
   }
-    @font-face {
+  @font-face {
     font-family: 'Roboto';
     font-weight: 'bold';
-    src: url(${RobotoBold})  format('truetype'),
+    src: url(${fontFiles.RobotoBold}) format('truetype');
   }
-    @font-face {
+  @font-face {
     font-family: 'Roboto';
     font-weight: 'medium';
-    src: url(${RobotoMedium})  format('truetype'),
+    src: url(${fontFiles.RobotoMedium}) format('truetype');
   }
 
-    @font-face {
+  @font-face {
     font-family: 'Roboto';
     font-weight: 'normal';
-    src: url(${Roboto})  format('truetype'),
+    src: url(${fontFiles.Roboto}) format('truetype');
   }
 
-    @font-face {
+  @font-face {
     font-family: 'Roboto Condensed';
     font-weight: 'normal';
-    src: url(${RobotoCondensedRegular})  format('truetype'),
+    src: url(${fontFiles.RobotoCondensedRegular}) format('truetype');
   }
 
-    @font-face {
+  @font-face {
     font-family: 'Roboto Condensed';
     font-weight: 'normal';
     font-style: italic;
-    src: url(${RobotoCondensedRegularItalic})  format('truetype'),
+    src: url(${fontFiles.RobotoCondensedRegularItalic}) format('truetype');
   }
-  
+
   html{
     height: 100%;
     box-sizing: border-box;
---
+++ src/styles/fonts.js
@@ -0,0 +1,8 @@
+import BebasNeueBold from '../assets/fonts/BebasNeue-Bold.ttf';
+import RobotoBold from '../assets/fonts/Roboto-Bold.ttf';
+import RobotoMedium from '../assets/fonts/Roboto-Medium.ttf';
+import Roboto from '../assets/fonts/Roboto-Regular.ttf';
+import RobotoCondensedRegular from '../assets/fonts/RobotoCondensed-Regular.ttf';
+import RobotoCondensedRegularItalic from '../assets/fonts/RobotoCondensed-RegularItalic.ttf';
+
+export { BebasNeueBold, RobotoBold, RobotoMedium, Roboto, RobotoCondensedRegular, RobotoCondensedRegularItalic };
