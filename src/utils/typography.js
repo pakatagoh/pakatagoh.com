24f7a30a433be4a3f86f0d1c905dbcc2558cd3cb
Pakata Goh <pakatagohlh@gmail.com>
2019-08-08 19:15:36 +0800

    add overrideStyles for nav to use same fonts as header

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -5,5 +5,10 @@ const typography = new Typography({
   baseLineHeight: 1.666,
   headerFontFamily: ['Bebas Neue', 'Segoe UI', 'Arial', 'sans-serif'],
   bodyFontFamily: ['Roboto', 'Georgia', 'serif'],
+  overrideStyles: () => ({
+    nav: {
+      fontFamily: ['Bebas Neue', 'Segoe UI', 'Arial', 'sans-serif'].join(','),
+    },
+  }),
 });
 export default typography;
