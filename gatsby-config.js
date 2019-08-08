9eba1337079ef087cc4f21a7093ffe84300efb71
Pakata Goh <pakatagohlh@gmail.com>
2019-08-08 12:41:25 +0800

    add gatsby-plugin-eslint to plugins list

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,7 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    'gatsby-plugin-eslint',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
