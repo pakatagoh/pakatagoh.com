43fdc11592a13d42ba783d3f449ab2eebb4bfee8
Pakata Goh <pakatagohlh@gmail.com>
2019-08-09 01:52:20 +0800

    remove fontface imports

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,48 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
-import * as fontFiles from './fonts';
 
 const GlobalStyle = createGlobalStyle`
-  @font-face {
-    font-family: 'Bebas Neue';
-    font-weight: 'bold';
-    src: url(${fontFiles.BebasNeueBold}) format('truetype');
-  }
-
-  @font-face {
-    font-family: 'Bebas Neue Book';
-    font-weight: 'normal';
-    src: url(${fontFiles.BebasNeueBook}) format('truetype');
-  }
-
-  @font-face {
-    font-family: 'Roboto';
-    font-weight: 'bold';
-    src: url(${fontFiles.RobotoBold}) format('truetype');
-  }
-  @font-face {
-    font-family: 'Roboto';
-    font-weight: 'medium';
-    src: url(${fontFiles.RobotoMedium}) format('truetype');
-  }
-
-  @font-face {
-    font-family: 'Roboto';
-    font-weight: 'normal';
-    src: url(${fontFiles.Roboto}) format('truetype');
-  }
-
-  @font-face {
-    font-family: 'Roboto Condensed';
-    font-weight: 'normal';
-    src: url(${fontFiles.RobotoCondensedRegular}) format('truetype');
-  }
-
-  @font-face {
-    font-family: 'Roboto Condensed';
-    font-weight: 'normal';
-    font-style: italic;
-    src: url(${fontFiles.RobotoCondensedRegularItalic}) format('truetype');
-  }
 
   html{
     height: 100%;
