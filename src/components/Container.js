7fcf7f41f87ee078dc855902954b0a6a865d34b8
Pakata Goh <pakatagohlh@gmail.com>
2019-08-09 00:21:13 +0800

    add BebasNeue-Book font

---
+++ src/components/Container.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import { media } from '../styles/sizes';
+
+const StyledWrapper = styled.div`
+  margin: 0 auto;
+  padding: 0 15px;
+  ${media.sm`max-width: 540px`}
+  ${media.md`max-width: 768px`}
+  ${media.lg`max-width: 992px`}
+  ${media.xl`max-width: 1200px`}
+`;
+
+const Container = ({ children }) => {
+  return <StyledWrapper>{children}</StyledWrapper>;
+};
+
+Container.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Container;
