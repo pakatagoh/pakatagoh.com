f37233b9ae03b63e7d860df2f6dc729ec199acc4
Pakata Goh <pakatagohlh@gmail.com>
2019-08-08 22:00:29 +0800

    delete default page-2 file and create about, blog, projects and contact pages

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const About = () => {
+  return (
+    <Layout>
+      <div>Hello from About page</div>
+    </Layout>
+  );
+};
+
+export default About;
