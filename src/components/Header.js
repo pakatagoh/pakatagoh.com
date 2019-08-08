226299a927c176a324679a06e6d45045625b52a1
Pakata Goh <pakatagohlh@gmail.com>
2019-08-09 00:25:11 +0800

    Add styling to navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,17 +2,20 @@ import { Link } from 'gatsby';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import React from 'react';
+import Container from './Container';
+import { media } from '../styles/sizes';
+import logo from '../images/Logo.svg';
 
 const ACTIVE = 'active';
 const NAVLINKS = ['projects', 'blog', 'about', 'contact'];
 
 const StyledNav = styled.nav`
+  height: 115px;
   display: flex;
   justify-content: space-between;
   align-items: center;
 
   & a {
-    padding-bottom: 6px;
     color: ${({ theme }) => theme.black};
     text-decoration: none;
   }
@@ -22,25 +25,64 @@ const StyledNav = styled.nav`
 `;
 
 const StyledBrandLink = styled(Link)`
+  display: flex;
+  align-items: center;
+`;
+
+const StyledBrandLogo = styled.div`
+  width: 45px;
+  height: 45px;
+`;
+
+const StyledImg = styled.img`
+  margin: 0;
+`;
+
+const StyledBrandTitle = styled.span`
+  font-family: 'Bebas Neue', 'Segoe UI', 'Arial', 'sans-serif';
   font-size: 38px;
+  margin-left: 15px;
+  display: none;
+
+  ${media.md`
+    display: block
+  `}
 `;
 
 const StyledLink = styled(Link)`
-  font-size: 24px;
+  font-size: 18px;
+  padding-bottom: 6px;
+  margin-left: 18px;
+
+  ${media.sm`
+    font-size: 24px;
+    margin-left: 30px
+  `}
+
+  ${media.md`
+    margin-left: 60px
+  `}
 `;
 
 const Header = ({ siteTitle }) => (
   <header>
-    <StyledNav>
-      <StyledBrandLink to="/">{siteTitle}</StyledBrandLink>
-      <div>
-        {NAVLINKS.map(navlink => (
-          <StyledLink key={navlink} to={`/${navlink}`} activeClassName={ACTIVE}>
-            {navlink}
-          </StyledLink>
-        ))}
-      </div>
-    </StyledNav>
+    <Container>
+      <StyledNav>
+        <StyledBrandLink to="/">
+          <StyledBrandLogo>
+            <StyledImg src={logo} alt={siteTitle} />
+          </StyledBrandLogo>
+          <StyledBrandTitle>{siteTitle}</StyledBrandTitle>
+        </StyledBrandLink>
+        <div>
+          {NAVLINKS.map(navlink => (
+            <StyledLink key={navlink} to={`/${navlink}`} activeClassName={ACTIVE}>
+              {navlink}
+            </StyledLink>
+          ))}
+        </div>
+      </StyledNav>
+    </Container>
   </header>
 );
 
