c52f7d163cfb729ea9d15fbc815cc8e00227e06c
Pakata Goh <pakatagohlh@gmail.com>
2019-08-09 00:23:04 +0800

    import and export BebasNeueBook font

--- src/styles/fonts.js
+++ src/styles/fonts.js
@@ -1,8 +1,17 @@
 import BebasNeueBold from '../assets/fonts/BebasNeue-Bold.ttf';
+import BebasNeueBook from '../assets/fonts/BebasNeue-Book.ttf';
 import RobotoBold from '../assets/fonts/Roboto-Bold.ttf';
 import RobotoMedium from '../assets/fonts/Roboto-Medium.ttf';
 import Roboto from '../assets/fonts/Roboto-Regular.ttf';
 import RobotoCondensedRegular from '../assets/fonts/RobotoCondensed-Regular.ttf';
 import RobotoCondensedRegularItalic from '../assets/fonts/RobotoCondensed-RegularItalic.ttf';
 
-export { BebasNeueBold, RobotoBold, RobotoMedium, Roboto, RobotoCondensedRegular, RobotoCondensedRegularItalic };
+export {
+  BebasNeueBold,
+  BebasNeueBook,
+  RobotoBold,
+  RobotoMedium,
+  Roboto,
+  RobotoCondensedRegular,
+  RobotoCondensedRegularItalic,
+};
