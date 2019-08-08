b408e230317c6e54c6c835d0b2a729bb2ac14872
Pakata Goh <pakatagohlh@gmail.com>
2019-08-08 17:43:25 +0800

    add basic styled components for navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,36 @@
 import { Link } from 'gatsby';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import React from 'react';
 
+const ACTIVE = 'active';
+
+const StyledNav = styled.nav`
+  font-family: 'Bebas Neue';
+`;
+
+const StyledBrand = styled(Link)`
+  color: black;
+  text-decoration: none;
+`;
+
+const StyledLink = styled(Link)`
+  color: black;
+  text-decoration: none;
+
+  &.${ACTIVE} {
+    border-bottom: 2px solid blue;
+  }
+`;
+
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div>
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+  <header>
+    <StyledNav>
+      <StyledBrand to="/">{siteTitle}</StyledBrand>
+      <StyledLink to="/page-2" activeClassName={ACTIVE}>
+        page2
+      </StyledLink>
+    </StyledNav>
   </header>
 );
 
