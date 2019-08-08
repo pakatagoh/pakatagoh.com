861d9fe07eeb8a4209c1471d0527f01d253c51af
Pakata Goh <pakatagohlh@gmail.com>
2019-08-08 22:01:59 +0800

    delete link to page-2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { Link } from 'gatsby';
-
 import Layout from '../components/Layout';
 import Image from '../components/image';
 import SEO from '../components/seo';
@@ -14,7 +12,6 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 );
 
