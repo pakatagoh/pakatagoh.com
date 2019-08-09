2a8d2536450912511731793a94cae7675fbcf9c5
Pakata Goh <pakatagohlh@gmail.com>
2019-08-09 13:58:13 +0800

    add StyledPageWrapper to push footer to the bottom of page

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import '../styles/fonts.css';
 import GlobalStyle from '../styles/GlobalStyle';
@@ -6,14 +7,28 @@ import StyledTheme from '../styles/StyledTheme';
 import Header from './Header';
 import Footer from './Footer';
 
+const StyledPageWrapper = styled.div`
+  position: relative;
+  min-height: 100vh;
+
+  & footer {
+    position: absolute;
+    bottom: 0;
+  }
+`;
+
 const Layout = ({ children }) => {
   return (
     <>
       <GlobalStyle />
       <StyledTheme>
-        <Header />
-        <main>{children}</main>
-        <Footer />
+        <StyledPageWrapper>
+          <Header />
+          <main>{children}</main>
+          <footer>
+            <Footer />
+          </footer>
+        </StyledPageWrapper>
       </StyledTheme>
     </>
   );
