5874eec10f1b36562c50d214e4b88fb0ef01fbee
Pakata Goh <pakatagohlh@gmail.com>
2019-08-09 21:12:08 +0800

    add scaleRatio to increase size of fonts

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -2,6 +2,7 @@ import Typography from 'typography';
 
 const typography = new Typography({
   baseFontSize: '18px',
+  scaleRatio: 3.33,
   baseLineHeight: 1.666,
   headerFontFamily: ['Bebas Neue', 'Segoe UI', 'Arial', 'sans-serif'],
   bodyFontFamily: ['Roboto', 'Georgia', 'serif'],
