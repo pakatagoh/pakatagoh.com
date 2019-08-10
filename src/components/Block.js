fd6e580fd214ba3a7a72a675542425cd186e4de4
Pakata Goh <pakatagohlh@gmail.com>
2019-08-10 22:49:00 +0800

    decrease block padding at smaller viewport

--- src/components/Block.js
+++ src/components/Block.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { media } from '../styles/sizes';
 
 const StyledWrapper = styled.div`
-  padding: 50px 0;
+  padding: 30px 0;
+
+  ${media.sm`
+    padding: 50px 0;
+  `};
 `;
 
 const Block = ({ children }) => {
