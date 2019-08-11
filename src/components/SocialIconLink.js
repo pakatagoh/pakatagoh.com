c02c8538299529399fe68a1e2ea2bedb484be5a2
Pakata Goh <pakatagohlh@gmail.com>
2019-08-11 14:53:23 +0800

    Rename to SocialIconLink and implement Icon inside

--- src/components/SocialIconLink.js
+++ src/components/SocialIconLink.js
@@ -2,17 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { media } from '../styles/sizes';
-import Icon from './Icon';
 
 const ICON_LINK_SIZE = {
   xs: '32px',
   sm: '38px',
 };
 
+const ICON_WRAPPER_SIZE = {
+  xs: '24px',
+  sm: '27px',
+};
+
 const StyledIconLink = styled.a`
   width: ${ICON_LINK_SIZE.xs};
   height: ${ICON_LINK_SIZE.xs};
-  margin-right: 20px;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -30,14 +33,34 @@ const StyledIconLink = styled.a`
   ${media.sm`
     width: ${ICON_LINK_SIZE.sm};
     height:${ICON_LINK_SIZE.sm};
-    margin: 0 15px;
   `};
+`;
+
+const StyledIconWrapper = styled.div`
+  width: ${ICON_WRAPPER_SIZE.xs};
+  height: ${ICON_WRAPPER_SIZE.xs};
+  display: inline-block;
+
+  ${media.sm`
+    width: ${ICON_WRAPPER_SIZE.sm};
+    height: ${ICON_WRAPPER_SIZE.sm};
+  `};
+`;
+
+const StyledIcon = styled.img`
+  width: 100%;
+  height: 100%;
+  object-position: center;
+  object-fit: contain;
+  margin: 0;
 `;
 
 const IconLink = ({ icon }) => {
   return (
-    <StyledIconLink href={icon.to} target="_blank" rel="noreferrer noopener">
-      <Icon src={icon.src} alt={icon.name} />
+    <StyledIconLink href={icon.to} target="_blank" rel="noreferrer noopener" aria-label={icon.name}>
+      <StyledIconWrapper>
+        <StyledIcon src={icon.src} alt={icon.name} />
+      </StyledIconWrapper>
     </StyledIconLink>
   );
 };
