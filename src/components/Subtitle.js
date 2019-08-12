34a51186e2e75161564be1ffe0adbd0174e50fc8
Pakata Goh <pakatagohlh@gmail.com>
2019-08-12 20:49:49 +0800

    Add Subtitle component for subtitle below headings

---
+++ src/components/Subtitle.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { media } from '../styles/sizes';
+
+const StyledSubtitle = styled.p`
+  font-size: 1.05rem;
+  line-height: 1.33;
+  margin-bottom: 1rem;
+
+  ${media.sm`
+    font-size: 1.25rem;
+    line-height: 1.45;
+    margin-bottom: 1.2rem;
+  `}
+
+  ${media.lg`
+    font-size: 1.33rem;
+    line-height: 1.6;
+    margin-bottom: 1.6rem;
+  `}
+`;
+
+const Subtitle = ({ className, children }) => {
+  return <StyledSubtitle className={className}>{children}</StyledSubtitle>;
+};
+
+Subtitle.propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+};
+
+export default Subtitle;
