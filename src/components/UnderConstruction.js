6ae61a104358cc538f3eea5145670fd959bb66a6
Pakata Goh <pakatagohlh@gmail.com>
2019-08-12 21:10:59 +0800

    create UnderConstruction component to show page is not ready

---
+++ src/components/UnderConstruction.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const UnderConstruction = () => {
+  return (
+    <p>
+      Under construction...
+      <span role="img" aria-label="sad face emoji">
+        😥
+      </span>
+    </p>
+  );
+};
+
+export default UnderConstruction;
