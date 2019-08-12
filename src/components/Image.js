adacdeb8e52d5921002ac8a2010e9e1ffb4dd2c2
Pakata Goh <pakatagohlh@gmail.com>
2019-08-12 20:48:58 +0800

    Add Image component with gatsby image

---
+++ src/components/Image.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Img from 'gatsby-image';
+import styled from 'styled-components';
+
+const StyledGatsbyImg = styled(Img)`
+  & img {
+    margin-bottom: 0;
+  }
+`;
+
+const Image = props => {
+  return <StyledGatsbyImg {...props} />;
+};
+
+export default Image;
