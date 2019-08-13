8c3ed4d991aaebadc54d6ade6ed8adabebfa27e8
Pakata Goh <pakatagohlh@gmail.com>
2019-08-13 23:59:13 +0800

    Add baseHeaderScale from typography.js

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -15,6 +15,15 @@ const typography = new Typography({
 
 const { rhythm, scale, options } = typography;
 
-export { rhythm, scale, options };
+// @link: https://github.com/KyleAMathews/typography.js/blob/49363c455d931f2fceaf7b7594c2c5159c1dc416/packages/typography/src/utils/createStyles.js#L248
+const baseHeaderScale = {
+  h1: 1,
+  h2: 3 / 5,
+  h3: 2 / 5,
+  h4: 0 / 5,
+  h5: -1 / 5,
+  h6: -1.5 / 5,
+};
 
+export { rhythm, scale, options, baseHeaderScale };
 export default typography;
