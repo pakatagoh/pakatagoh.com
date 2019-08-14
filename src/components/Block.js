a3c78cb0408a75eaa59aee0abe961a76a6ef3aec
Pakata Goh <pakatagohlh@gmail.com>
2019-08-14 12:56:46 +0800

    Use rhythm instead of px

--- src/components/Block.js
+++ src/components/Block.js
@@ -2,12 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { media } from '../styles/sizes';
+import { rhythm } from '../utils/typography';
 
 const StyledWrapper = styled.div`
-  padding: 30px 0;
+  padding-top: ${rhythm(2 / 3)};
+  padding-bottom: ${rhythm(2 / 3)};
 
   ${media.sm`
-    padding: 50px 0;
+  padding-top: ${rhythm(3 / 3)};
+  padding-bottom: ${rhythm(3 / 3)};
   `};
 `;
 
