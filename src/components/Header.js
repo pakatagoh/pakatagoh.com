fffbcc97c38912ba57786d3ecab85d83630375b7
Pakata Goh <pakatagohlh@gmail.com>
2019-08-15 23:35:44 +0800

    Add anchor css reset for StyledLink

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ import Container from './Container';
 import { media } from '../styles/sizes';
 import { rhythm } from '../utils/typography';
 import logo from '../assets/icons/Logo.svg';
+import { StyledCSSLinkReset } from '../styles/GlobalStyle';
 
 const NAVLINKS = ['blog', 'about', 'contact'];
 const ACTIVE = 'active';
@@ -27,6 +28,7 @@ const StyledNav = styled.nav`
 const StyledBrandLink = styled(Link)`
   display: flex;
   align-items: center;
+  ${StyledCSSLinkReset}
 `;
 
 const StyledBrandLogo = styled.div`
@@ -56,6 +58,7 @@ const StyledLink = styled(Link)`
   padding: ${rhythm(1 / 5)} 0;
   margin-left: ${rhythm(3 / 5)};
   color: ${({ theme }) => theme.gray2};
+  ${StyledCSSLinkReset}
 
   &:hover {
     color: ${({ theme }) => theme.black};
