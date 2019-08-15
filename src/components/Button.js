152a2dc942682779a8f0604f0fefeb44f1225df6
Pakata Goh <pakatagohlh@gmail.com>
2019-08-15 21:42:50 +0800

    use display flex for StyledButton

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,7 +5,8 @@ import { media } from '../styles/sizes';
 
 const StyledButton = styled.div`
   padding: 6px 12px;
-  display: inline-block;
+  display: flex;
+  align-items: center;
   border: none;
   background-color: ${({ color, theme }) => (theme[color] ? theme[color].base : theme.primary.base)};
   box-shadow: none;
