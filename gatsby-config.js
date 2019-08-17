cbeffa0c640ad275f2b31f5305a70726cb8b7344
Pakata Goh <pakatagohlh@gmail.com>
2019-08-17 22:15:49 +0800

    Install and configure gatsby-plugin-twitter

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,6 +58,7 @@ module.exports = {
       },
     },
     'gatsby-plugin-eslint',
+    'gatsby-plugin-twitter',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
