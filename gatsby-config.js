2e66bf2040f5e0de6a61eb23871b591a2c9a8094
Pakata Goh <pakatagohlh@gmail.com>
2019-08-19 02:24:13 +0800

    change logo to jpg format

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,7 +67,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/Logo.png`, // This path is relative to the root of the site.
+        icon: `src/images/Logo.jpg`, // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-styled-components',
