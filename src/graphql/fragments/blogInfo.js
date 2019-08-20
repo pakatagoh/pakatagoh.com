e4d78c105c5b949bd459603f919b326e6aced4b7
Pakata Goh <pakatagohlh@gmail.com>
2019-08-21 00:53:13 +0800

    create fragment for querying blog info

---
+++ src/graphql/fragments/blogInfo.js
@@ -0,0 +1,21 @@
+import { graphql } from 'gatsby';
+
+// eslint-disable-next-line import/prefer-default-export
+export const query = graphql`
+  fragment BlogInfo on MdxConnection {
+    edges {
+      node {
+        id
+        excerpt(pruneLength: 150)
+        fields {
+          slug
+        }
+        frontmatter {
+          createdAt(formatString: "YYYY-MM-DD")
+          title
+          updatedAt(formatString: "YYYY-MM-DD")
+        }
+      }
+    }
+  }
+`;
