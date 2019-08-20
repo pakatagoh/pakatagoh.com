d01a35dd8e3f50cbfcd7336a48ca59a617d065a5
Pakata Goh <pakatagohlh@gmail.com>
2019-08-20 22:36:55 +0800

    remove @ from twitter handle and add github repo

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ const config = {
     link: 'https://www.instagram.com/paka.codes/',
   },
   twitter: {
-    handle: '@GohPakata',
+    handle: 'GohPakata',
     link: 'https://twitter.com/gohpakata',
   },
   site: {
@@ -19,6 +19,9 @@ const config = {
     url: 'https://pakatagoh.com',
     description:
       "Pakata is a software developer from Singapore specializing in JavaScript. In this personal site, he writes about things he's learnt and hopefully you'll learn a thing or two from it.",
+  },
+  repo: {
+    link: 'https://github.com/pakatagoh/pakatagoh.com',
   },
   googleAnalytics: {
     trackingId: 'UA-146042974-1',
