7892c28c3e8613fdaca7e492c523fc35a4f96996
Pakata Goh <pakatagohlh@gmail.com>
2019-08-20 22:22:37 +0800

    create config file with constants

---
+++ config/index.js
@@ -0,0 +1,28 @@
+const config = {
+  name: 'Pakata Goh',
+  github: 'https://github.com/pakatagoh',
+  linkedin: 'https://www.linkedin.com/in/pakata-goh/',
+  instagram_personal: {
+    handle: '@pakatagoh',
+    link: 'https://www.instagram.com/pakatagoh/',
+  },
+  instagram_coding: {
+    handle: '@paka.codes',
+    link: 'https://www.instagram.com/paka.codes/',
+  },
+  twitter: {
+    handle: '@GohPakata',
+    link: 'https://twitter.com/gohpakata',
+  },
+  site: {
+    title: 'Pakata Goh',
+    url: 'https://pakatagoh.com',
+    description:
+      "Pakata is a software developer from Singapore specializing in JavaScript. In this personal site, he writes about things he's learnt and hopefully you'll learn a thing or two from it.",
+  },
+  googleAnalytics: {
+    trackingId: 'UA-146042974-1',
+  },
+};
+
+module.exports = config;
