0b6648e626f3fec9c408ec487057d13f7f8d009a
Pakata Goh <pakatagohlh@gmail.com>
2019-08-21 22:08:04 +0800

    Extract blog list item into separate component and use it

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,62 +1,10 @@
 import React from 'react';
-import { useStaticQuery, graphql, Link } from 'gatsby';
-import styled from 'styled-components';
-import { rhythm } from '../utils/typography';
+import { useStaticQuery, graphql } from 'gatsby';
 import Layout from '../components/Layout';
 import Container from '../components/Container';
 import PageTitle from '../components/PageTitle';
+import BlogListItem from '../components/BlogListItem';
 import SEO from '../components/SEO';
-import formatDate from '../utils/formatDate';
-import Small from '../components/Small';
-
-const StyledArticle = styled.article`
-  margin-bottom: ${rhythm(1)};
-`;
-
-const StyledSmall = styled(Small)`
-  color: ${({ theme }) => theme.gray2};
-`;
-
-const StyledPostTitle = styled.h2`
-  margin-bottom: ${rhythm(1 / 5)};
-
-  & a {
-    font-size: 1em;
-    color: ${({ theme }) => theme.black};
-    text-decoration: none;
-
-    &:hover {
-      color: ${({ theme }) => theme.primary.hover};
-      border-bottom: 1px solid ${({ theme }) => theme.primary.hover};
-      background-color: ${({ theme }) => theme.white2};
-    }
-    &:active {
-      background-color: ${({ theme }) => theme.white};
-      border-bottom: 1px solid ${({ theme }) => theme.primary.pressed};
-      color: ${({ theme }) => theme.primary.pressed};
-    }
-  }
-`;
-
-const StyledLink = styled(Link)`
-  color: ${({ theme }) => theme.primary.base};
-  text-decoration: none;
-
-  &:hover {
-    color: ${({ theme }) => theme.primary.hover};
-    border-bottom: 1px solid ${({ theme }) => theme.primary.hover};
-    background-color: ${({ theme }) => theme.white2};
-  }
-  &:active {
-    background-color: ${({ theme }) => theme.white};
-    border-bottom: 1px solid ${({ theme }) => theme.primary.pressed};
-    color: ${({ theme }) => theme.primary.pressed};
-  }
-`;
-
-const StyledExcerpt = styled.p`
-  margin-bottom: 0;
-`;
 
 const Blog = () => {
   const data = useStaticQuery(graphql`
@@ -80,34 +28,14 @@ const Blog = () => {
           const { excerpt, fields, id, frontmatter } = post;
           const { slug } = fields;
           const { title, createdAt, updatedAt } = frontmatter;
-
-          return (
-            <StyledArticle key={id}>
-              <StyledPostTitle>
-                <Link to={slug}>{title}</Link>
-              </StyledPostTitle>
-              <StyledExcerpt>
-                {excerpt}
-                <StyledLink to={slug}>
-                  READ POST <i className="icon-arrow_right_solid" />
-                </StyledLink>
-              </StyledExcerpt>
-              <div>
-                <StyledSmall>Posted: </StyledSmall>
-                <StyledSmall forwardedAs="time" dateTime={updatedAt}>
-                  {formatDate(createdAt)}
-                </StyledSmall>
-                {updatedAt && (
-                  <>
-                    <StyledSmall> / Updated: </StyledSmall>
-                    <StyledSmall forwardedAs="time" dateTime={updatedAt}>
-                      {formatDate(updatedAt)}
-                    </StyledSmall>
-                  </>
-                )}
-              </div>
-            </StyledArticle>
-          );
+          const blogListItemProps = {
+            excerpt,
+            slug,
+            title,
+            createdAt,
+            updatedAt,
+          };
+          return <BlogListItem key={id} {...blogListItemProps} />;
         })}
       </Container>
     </Layout>
