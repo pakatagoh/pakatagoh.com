8a303a995fbd1ef7b7e7d39c6aae9c272f9095a6
Pakata Goh <pakatagohlh@gmail.com>
2019-08-22 15:24:20 +0800

    add logic for searching

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
+import styled from 'styled-components';
+import matchSorter from 'match-sorter';
 import Layout from '../components/Layout';
 import Container from '../components/Container';
 import PageTitle from '../components/PageTitle';
 import BlogListItem from '../components/BlogListItem';
 import SEO from '../components/SEO';
+
+const StyledInput = styled.input`
+  width: 100%;
+  padding: 2px 4px;
+  margin-bottom: 1rem;
+  color: ${({ theme }) => theme.black};
+  border: 1px solid ${({ theme }) => theme.gray2};
+  outline: none;
+
+  &::placeholder {
+    color: ${({ theme }) => theme.gray1};
+  }
+
+  &:hover {
+    &::placeholder {
+      color: ${({ theme }) => theme.black};
+    }
+    border-color: ${({ theme }) => theme.gray1};
+  }
+
+  &:focus {
+    &::placeholder {
+      color: ${({ theme }) => theme.black};
+    }
+    border-color: ${({ theme }) => theme.primary.pressed};
+  }
+`;
 
 const Blog = () => {
   const data = useStaticQuery(graphql`
@@ -18,17 +47,44 @@ const Blog = () => {
     }
   `);
 
-  const { edges: posts } = data.allMdx;
+  const { edges } = data.allMdx;
+  const blogPosts = React.useMemo(() => {
+    return edges.map(({ node: post }) => {
+      const { id, excerpt } = post;
+      return {
+        id,
+        excerpt,
+        ...post.fields,
+        ...post.frontmatter,
+      };
+    });
+  }, [edges]);
+  const [posts, setPosts] = useState(blogPosts);
+  const [filterVal, setFilterVal] = useState('');
+
+  const handleInputChange = e => {
+    const searchTerm = e.target.value;
+    setFilterVal(searchTerm);
+
+    if (!searchTerm) {
+      setPosts(blogPosts);
+      return;
+    }
+    const filteredPosts = matchSorter(blogPosts, searchTerm, {
+      keys: ['title', 'keywords', { key: 'excerpt', threshold: matchSorter.rankings.CONTAINS }],
+    });
+
+    setPosts(filteredPosts);
+  };
+
   return (
     <Layout>
       <SEO title="Blog" />
       <Container>
         <PageTitle block>BLOG</PageTitle>
+        <StyledInput type="text" value={filterVal} onChange={handleInputChange} placeholder="Search posts" />
         {posts.length > 0 ? (
-          posts.map(({ node: post }) => {
-            const { excerpt, fields, id, frontmatter } = post;
-            const { slug } = fields;
-            const { title, createdAt, updatedAt } = frontmatter;
+          posts.map(({ id, excerpt, slug, title, createdAt, updatedAt }) => {
             const blogListItemProps = {
               excerpt,
               slug,
