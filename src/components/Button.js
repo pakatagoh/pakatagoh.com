79140d4709a76566e3ead58422449989551d4b6e
Pakata Goh <pakatagohlh@gmail.com>
2020-02-10 00:53:21 +0800

    use react-spring handle button tap animation mobile

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,30 +1,29 @@
 import React from 'react';
+import { animated, useSpring } from 'react-spring';
+import styled, { css, useTheme } from 'styled-components';
 import PropTypes from 'prop-types';
-import styled, { css } from 'styled-components';
 import { media } from '../styles/sizes';
 import isMobile from '../utils/isMobile';
 
-const StyledButton = styled.button`
+const StyledButton = styled(animated.button)`
   padding: 6px 12px;
   display: inline-flex;
   align-items: center;
   border: none;
-  background-color: ${({ color, theme }) => (theme[color] ? theme[color].base : theme.primary.base)};
-  box-shadow: none;
-  transition: box-shadow 0.1s linear;
-
   ${!isMobile() &&
     css`
+      background-color: ${({ color, theme }) => (theme[color] ? theme[color].base : theme.primary.base)};
+      box-shadow: none;
+      transition: box-shadow 0.1s linear;
       &:hover {
         background-color: ${({ color, theme }) => (theme[color] ? theme[color].hover : theme.primary.hover)};
         box-shadow: ${({ theme }) => theme.shadow.hover};
       }
+      &:active {
+        background-color: ${({ color, theme }) => (theme[color] ? theme[color].pressed : theme.primary.pressed)};
+        box-shadow: ${({ theme }) => theme.shadow.pressed};
+      }
     `}
-
-  &:active {
-    background-color: ${({ color, theme }) => (theme[color] ? theme[color].pressed : theme.primary.pressed)};
-    box-shadow: ${({ theme }) => theme.shadow.pressed};
-  }
 
   ${media.sm`
     padding: 8px 15px;  
@@ -32,8 +31,39 @@ const StyledButton = styled.button`
 `;
 
 const Button = ({ children, color, onClick }) => {
+  const theme = useTheme();
+  const [spring, set] = useSpring(() => ({
+    to: {
+      boxShadow: theme.shadow.hover,
+      backgroundColor: theme[color] ? theme[color].hover : theme.primary.hover,
+    },
+  }));
+
+  const handleClick = () => {
+    if (isMobile) {
+      set({
+        to: {
+          boxShadow: theme.shadow.pressed,
+          backgroundColor: theme[color] ? theme[color].pressed : theme.primary.pressed,
+        },
+        config: {
+          duration: 80,
+        },
+        onRest: () => {
+          set({
+            boxShadow: theme.shadow.hover,
+            backgroundColor: theme[color] ? theme[color].hover : theme.primary.hover,
+          });
+        },
+      });
+    }
+    if (onClick) {
+      onClick();
+    }
+  };
+
   return (
-    <StyledButton color={color} onClick={onClick}>
+    <StyledButton color={color} onClick={handleClick} style={isMobile() ? spring : null}>
       {children}
     </StyledButton>
   );
