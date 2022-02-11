1e5783dfeb4afd00318c50c156fd76eefcae6bbc
pakata goh <pakatagohlh@gmail.com>
2022-02-11 18:49:14 +0800

    feat: update sanity schema

--- apps/cms/schemas/post.js
+++ apps/cms/schemas/post.js
@@ -1,56 +1,26 @@
 export default {
-  name: 'post',
-  title: 'Post',
-  type: 'document',
+  name: "post",
+  title: "Post",
+  type: "document",
   fields: [
     {
-      name: 'title',
-      title: 'Title',
-      type: 'string',
+      name: "title",
+      title: "Title",
+      type: "string",
     },
     {
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
+      name: "slug",
+      title: "Slug",
+      type: "slug",
       options: {
-        source: 'title',
+        source: "title",
       },
     },
     {
-      name: 'categories',
-      title: 'Categories',
-      type: 'array',
-      of: [{ type: 'reference', to: { type: 'category' } }],
-    },
-    {
-      name: 'publishedAt',
-      title: 'Published at',
-      type: 'date',
-    },
-    {
-      name: 'editedOn',
-      title: 'Edited on',
-      type: 'date',
-    },
-    {
-      name: 'body',
-      title: 'Body',
-      type: 'markdown',
-      description: 'A Github flavored markdown field with image uploading',
+      name: "body",
+      title: "Body",
+      type: "markdown",
+      description: "A Github flavored markdown field with image uploading",
     },
   ],
-
-  preview: {
-    select: {
-      title: 'title',
-      author: 'author.name',
-      media: 'mainImage',
-    },
-    prepare(selection) {
-      const { author } = selection;
-      return Object.assign({}, selection, {
-        subtitle: author && `by ${author}`,
-      });
-    },
-  },
 };
