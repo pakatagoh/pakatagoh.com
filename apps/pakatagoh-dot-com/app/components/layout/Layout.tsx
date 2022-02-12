d807043a6cd13226eeafbd954c615bd0c4283415
pakata goh <pakatagohlh@gmail.com>
2022-02-13 03:42:16 +0800

    chore: remove sanity and monorepo

--- apps/pakatagoh-dot-com/app/components/layout/Layout.tsx
+++
@@ -1,7 +0,0 @@
-import { PropsWithChildren } from "react";
-
-export const Layout = (props: PropsWithChildren<any>) => {
-  return (
-    <div className="px-4 py-3 md:mx-auto md:max-w-3xl">{props.children}</div>
-  );
-};
