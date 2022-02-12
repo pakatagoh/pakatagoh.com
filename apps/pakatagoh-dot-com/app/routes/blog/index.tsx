0640f638c63b472d08cf3db676023f0f1bc593ba
pakata goh <pakatagohlh@gmail.com>
2022-02-13 02:25:43 +0800

    feat: blog index page

--- apps/pakatagoh-dot-com/app/routes/blog/index.tsx
+++ apps/pakatagoh-dot-com/app/routes/blog/index.tsx
@@ -1,10 +1,10 @@
-import { json, useLoaderData, Link } from 'remix';
+import { json, useLoaderData, Link } from "remix";
 import type {
   HeadersFunction,
   LoaderFunction,
   ErrorBoundaryComponent,
-} from 'remix';
-import { getBlogPosts } from '../../blog';
+} from "remix";
+import { getBlogPosts } from "../../blog";
 
 // export const headers: HeadersFunction = ({ loaderHeaders }) => {
 //   // console.log('loaderHeaders: ', loaderHeaders.get('cache-control'));
@@ -19,35 +19,48 @@ import { getBlogPosts } from '../../blog';
 type LoaderData = {
   slug: string;
   title: string;
+  description?: string;
   createdAt: string;
 };
 
-// export const loader: LoaderFunction = async () => {
-//   const posts = await getBlogPosts();
+export const loader: LoaderFunction = async () => {
+  const posts = await getBlogPosts();
 
-//   return json(posts, {
-//     status: 200,
-//     // headers: {
-//     //   'cache-control':
-//     //     'max-age=1800, s-maxage=86400, stale-while-revalidate=31536000',
-//     // },
-//   });
-// };
+  return json(posts, {
+    status: 200,
+    headers: {
+      "cache-control":
+        "max-age=1800, s-maxage=86400, stale-while-revalidate=31536000",
+    },
+  });
+};
 
 const BlogIndex = () => {
-  // const data = useLoaderData<LoaderData[]>();
+  const data = useLoaderData<LoaderData[]>();
 
   return (
     <main>
-      hello from blog index
-      {/* {data.map((postItem) => {
-        return (
-          <div key={postItem.slug}>
-            <Link to={postItem.slug}>{postItem.title}</Link>
-            <div>{postItem.createdAt}</div>
-          </div>
-        );
-      })} */}
+      <h1 className="text-xl font-bold">Blog</h1>
+      <div className="mt-6 flex flex-col gap-4">
+        {data.map((postItem) => {
+          return (
+            <div key={postItem.slug}>
+              <div className="flex items-center gap-3">
+                <Link to={postItem.slug} className="font-medium">
+                  {postItem.title}
+                </Link>
+                <span> - </span>
+                <span className="text-sm text-gray-700">
+                  {postItem.createdAt}
+                </span>
+              </div>
+              <p className="mt-1 text-sm text-gray-700">
+                {postItem.description}
+              </p>
+            </div>
+          );
+        })}
+      </div>
     </main>
   );
 };
