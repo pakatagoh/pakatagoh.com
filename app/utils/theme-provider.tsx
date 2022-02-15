d87862cd40b21c12b81f3f0ef715bc2040de60e4
pakata goh <pakatagohlh@gmail.com>
2022-02-15 23:46:41 +0800

    feat: add theme related actions and context providers

---
+++ app/utils/theme-provider.tsx
@@ -0,0 +1,151 @@
+import {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  Dispatch,
+  useRef,
+} from "react";
+import { useFetcher } from "remix";
+
+export enum Theme {
+  DARK = "dark",
+  LIGHT = "light",
+}
+
+type ThemeContextType = {
+  theme?: Theme | null;
+  setTheme?: Dispatch<React.SetStateAction<Theme | null>>;
+};
+
+const ThemeContext = createContext<ThemeContextType>({});
+
+const themes: Array<Theme> = Object.values(Theme);
+
+const prefersLightMQ = "(prefers-color-scheme: light)";
+const getPreferredTheme = () =>
+  window.matchMedia(prefersLightMQ).matches ? Theme.LIGHT : Theme.DARK;
+
+export function ThemeProvider({
+  children,
+  specifiedTheme,
+}: {
+  children: React.ReactNode;
+  specifiedTheme?: Theme | null;
+}) {
+  const [theme, setTheme] = useState<Theme | null>(() => {
+    // On the server, if we don't have a specified theme then we should
+    // return null and the clientThemeCode will set the theme for us
+    // before hydration. Then (during hydration), this code will get the same
+    // value that clientThemeCode got so hydration is happy.
+    if (specifiedTheme) {
+      if (themes.includes(specifiedTheme)) return specifiedTheme;
+      else return null;
+    }
+
+    // there's no way for us to know what the theme should be in this context
+    // the client will have to figure it out before hydration.
+    if (typeof window !== "object") return null;
+
+    return getPreferredTheme();
+  });
+
+  const persistTheme = useFetcher();
+  // TODO: remove this when persistTheme is memoized properly
+  const persistThemeRef = useRef(persistTheme);
+  useEffect(() => {
+    persistThemeRef.current = persistTheme;
+  }, [persistTheme]);
+
+  const mountRun = useRef(false);
+
+  useEffect(() => {
+    if (!mountRun.current) {
+      mountRun.current = true;
+      return;
+    }
+    if (!theme) return;
+
+    persistThemeRef.current.submit(
+      { theme },
+      { action: "action/set-theme", method: "post" }
+    );
+  }, [theme]);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia(prefersLightMQ);
+    const handleChange = () => {
+      setTheme(mediaQuery.matches ? Theme.LIGHT : Theme.DARK);
+    };
+    mediaQuery.addEventListener("change", handleChange);
+    return () => mediaQuery.removeEventListener("change", handleChange);
+  }, []);
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export const useTheme = () => {
+  const themeContext = useContext(ThemeContext);
+
+  if (themeContext === undefined) {
+    throw new Error("useTheme must be used within a ThemeProvider");
+  }
+
+  return themeContext;
+};
+
+const clientThemeCode = `
+// hi there dear reader 👋
+// this is how I make certain we avoid a flash of the wrong theme. If you select
+// a theme, then I'll know what you want in the future and you'll not see this
+// script anymore.
+;(() => {
+  const theme = window.matchMedia(${JSON.stringify(prefersLightMQ)}).matches
+    ? 'light'
+    : 'dark';
+  
+  const cl = document.documentElement.classList;
+  
+  const themeAlreadyApplied = cl.contains('light') || cl.contains('dark');
+  if (themeAlreadyApplied) {
+    // this script shouldn't exist if the theme is already applied!
+    console.warn(
+      "Hi there, could you let Kent know you're seeing this message? Thanks!",
+    );
+  } else {
+    cl.add(theme);
+  }
+})();
+`;
+
+export function NonFlashOfWrongThemeEls({ ssrTheme }: { ssrTheme: boolean }) {
+  return (
+    <>
+      {/*
+        On the server, "theme" might be `null`, so clientThemeCode ensures that
+        this is correct before hydration.
+      */}
+      {/*
+        If we know what the theme is from the server then we don't need
+        to do fancy tricks prior to hydration to make things match.
+      */}
+      {ssrTheme ? null : (
+        <script
+          // NOTE: we cannot use type="module" because that automatically makes
+          // the script "defer". That doesn't work for us because we need
+          // this script to run synchronously before the rest of the document
+          // is finished loading.
+          dangerouslySetInnerHTML={{ __html: clientThemeCode }}
+        />
+      )}
+    </>
+  );
+}
+
+export function isTheme(value: unknown): value is Theme {
+  return typeof value === "string" && themes.includes(value as Theme);
+}
