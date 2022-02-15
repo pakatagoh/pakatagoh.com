19f4b3a7cfe793a256086a10016d27557f7e488b
pakata goh <pakatagohlh@gmail.com>
2022-02-15 23:46:59 +0800

    feat: add theme logic in root of app

--- app/root.tsx
+++ app/root.tsx
@@ -1,15 +1,23 @@
 import {
+  json,
   Links,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
-  LinksFunction,
   useCatch,
+  useLoaderData,
 } from "remix";
-import type { MetaFunction } from "remix";
+import type { MetaFunction, LoaderFunction, LinksFunction } from "remix";
 import styles from "./styles/app.css";
+import {
+  NonFlashOfWrongThemeEls,
+  Theme,
+  ThemeProvider,
+  useTheme,
+} from "./utils/theme-provider";
+import { getThemeSession } from "./utils/theme.server";
 
 export const meta: MetaFunction = () => {
   return { title: "Pakata Goh" };
@@ -19,16 +27,35 @@ export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: styles }];
 };
 
-export default function App() {
+type LoaderData = {
+  theme: Theme;
+};
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const themeSession = await getThemeSession(request);
+
+  const data: LoaderData = {
+    theme: themeSession.getTheme(),
+  };
+
+  return json(data);
+};
+
+function App() {
+  const { theme } = useTheme();
+
+  const isDark = theme === Theme.DARK;
+
   return (
-    <html lang="en">
+    <html lang="en" className={isDark ? "dark" : undefined}>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta charSet="utf-8" />
         <Meta />
         <Links />
+        <NonFlashOfWrongThemeEls ssrTheme={Boolean(theme)} />
       </head>
-      <body>
+      <body className="dark:bg-gray-800 dark:text-white">
         <Outlet />
         <ScrollRestoration />
         <Scripts />
@@ -38,20 +65,30 @@ export default function App() {
   );
 }
 
+export default function AppWithProviders() {
+  const data = useLoaderData<LoaderData>();
+
+  return (
+    <ThemeProvider specifiedTheme={data.theme}>
+      <App />
+    </ThemeProvider>
+  );
+}
 export function CatchBoundary({ error }: { error: Error }) {
   const caught = useCatch();
 
   const isFourOhFour = caught.status === 404;
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta charSet="utf-8" />
         <title>Not Found</title>
         <Meta />
         <Links />
+        <NonFlashOfWrongThemeEls ssrTheme={false} />
       </head>
-      <body>
+      <body className="dark:bg-gray-800 dark:text-white">
         <div>
           {isFourOhFour ? (
             <>
@@ -77,15 +114,16 @@ export function CatchBoundary({ error }: { error: Error }) {
 
 export function ErrorBoundary({ error }: { error: Error }) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta charSet="utf-8" />
         <title>Uh-oh!</title>
         <Meta />
         <Links />
+        <NonFlashOfWrongThemeEls ssrTheme={false} />
       </head>
-      <body>
+      <body className="dark:bg-gray-800 dark:text-white">
         <div className="error-container">
           <h1>App Error</h1>
           <pre>{error.message}</pre>
