eea5348065dd0267c361ee1705fceb5207e78503
pakata goh <pakatagohlh@gmail.com>
2022-02-17 02:26:02 +0800

    feat: add custom typography theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,30 @@
 /* eslint-disable no-undef */
+
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}", "./content/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: [
+            {
+              "code::before": {
+                content: '""',
+              },
+              "code::after": {
+                content: '""',
+              },
+              pre: {
+                padding: 0,
+                boxShadow: theme("boxShadow.md"),
+                borderRadius: theme("borderRadius.lg"),
+              },
+            },
+          ],
+        },
+      }),
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
