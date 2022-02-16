8e015e53dc050411e97b0b5d00f53bb68487dc36
pakata goh <pakatagohlh@gmail.com>
2022-02-16 23:19:35 +0800

    hack: temporary hack for imports until remix team fixes issue

---
+++ app/es-modules.js
@@ -0,0 +1,5 @@
+/* eslint-disable no-undef */
+module.exports = {
+  importRehypeAutolinkHeadings: async () => import("rehype-autolink-headings"),
+  importRehypeSlug: async () => import("rehype-slug"),
+};
