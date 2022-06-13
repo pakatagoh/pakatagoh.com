e5a4b48176e6f95025bf011dca48683b5e6aeca0
Pakata Goh <pakatagohlh@gmail.com>
2022-06-13 11:22:07 +0800

    chore: update remix config

--- remix.config.js
+++ remix.config.js
@@ -4,7 +4,6 @@
 module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
   ignoredRouteFiles: [".*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
-  serverDependenciesToBundle: [/^rehype.*/, /^remark.*/, "mdx-bundler"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
