4ed8b76a2d8cf1923748cecec37b3af6fe73a17b
Pakata Goh <pakatagohlh@gmail.com>
2024-08-25 18:33:52 +0800

    initial api route

---
+++ src/pages/api/transaction.ts
@@ -0,0 +1,19 @@
+import type { APIRoute } from "astro";
+export const POST: APIRoute = async ({ request }) => {
+  if (request.headers.get("Content-Type") === "application/json") {
+    try {
+      const body = await request.json();
+
+      const name = body.name;
+
+      return new Response(
+        JSON.stringify({
+          greeting: "Hello" + name ?? "No name",
+        }),
+      );
+    } catch (error) {
+      return new Response(null, { status: 400 });
+    }
+  }
+  return new Response(null, { status: 400 });
+};
